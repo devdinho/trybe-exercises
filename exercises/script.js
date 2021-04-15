d240386e05cb50058b29a4c554e62e968ce83702
Diego Figueiredo <8kball@gmail.com>
2021-04-15 17:53:47 -0300

    Adiciona fetch de estados

--- exercises/script.js
+++ exercises/script.js
@@ -1,1 +1,15 @@
-console.log(1);
+const statesContainer = document.querySelector('#states-container');
+const URL = 'https://servicodados.ibge.gov.br/api/v1/localidades/estados';
+
+(async function generateData() {
+  const rawData = await fetch(URL);
+  const data = await rawData.json();
+  
+  const dataToSort = []
+  
+  data.forEach(element => {
+    dataToSort.push(`<option>${element.nome}</option>`)
+  })
+
+  statesContainer.innerHTML += dataToSort.sort().join('')
+})()
