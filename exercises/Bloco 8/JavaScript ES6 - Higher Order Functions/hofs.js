584a280e2b41b7543cb3c4f045836f4226e00039
Diego Figueirêdo <8kball@gmail.com>
2021-04-30 14:49:33 -0300

    HOFs

--- exercises/Bloco 8/JavaScript ES6 - Higher Order Functions/hofs.js
+++ exercises/Bloco 8/JavaScript ES6 - Higher Order Functions/hofs.js
@@ -1,9 +1,14 @@
 /*
-  Crie uma função de primeira classe que retorna um objeto { nomeCompleto, email }
-  de uma nova pessoa contratada. Passe sua função como parâmetro da HOF newEmployees
-  para criar cada pessoa contratada em seu respctivo id . A sua função deve receber
-  como parâmetro o nome completo da pessoa funcionária e a partir dele gerar automaticamente
-  um email no formato nome_da_pessoa@trybe.com .
+  1. Crie uma função de primeira classe que retorna um objeto { nomeCompleto, email }
+    de uma nova pessoa contratada. Passe sua função como parâmetro da HOF newEmployees
+    para criar cada pessoa contratada em seu respctivo id . A sua função deve receber
+    como parâmetro o nome completo da pessoa funcionária e a partir dele gerar automaticamente
+    um email no formato nome_da_pessoa@trybe.com.
+
+  2. Desenvolva uma HOF que retorna o resultado de um sorteio.
+    Esta HOF irá gerar um número aleatório entre 1 e 5 recebendo como parâmetros 
+    número apostado e uma função que checa se o número apostado é igual ao número sorteado.
+    O retorno da sua HOF deve ser uma string (Ex: "Tente novamente" ou "Parabéns você ganhou").
 */
 
 const newEmployees = () => {
